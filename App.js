6a86601a4143d399d7d4d34ceddff6c9bf217b1d
Yingying <yying023@outlook.com>
2023-09-14 14:11:22 -0700

    add header component and props

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
 
 
 export default function App() {
   const name = "yingying";
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app on {name}!</Text>
+      {/* <Text>Open up App.js to start working on your app on {name}!</Text> */}
+      <Header appName={name}/>
       <StatusBar style="auto" />
     </View>
   );
