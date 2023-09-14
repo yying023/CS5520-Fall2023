ea9bb37dd27a2dcb075a8346664404a72771e81c
Yingying <yying023@outlook.com>
2023-09-14 13:33:02 -0700

    123

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const Welcome = ({name}) => {
+    console.log(props)
+}
+
 export default function App() {
   return (
+    // <Welcome name="Sara" />;
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
